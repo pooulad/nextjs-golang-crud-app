44a130e615fd06eeceb392374505eefd247600fa
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-07 16:10:40 +0330

    Create crete_users_table.go

---
+++ database/migrations/crete_users_table.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Users struct {
+	ID      uint      `gorm:"primarykey;autoIncrement" json:"id"`
+	Name    *string   `json:"name"`
+	Email   *string   `json:"email"`
+	Date    time.Time `json:"date"`
+	City    *string   `json:"city"`
+	Country *string   `json:"country"`
+}
+
+func MigrateUsers(db *gorm.DB) error {
+	err := db.AutoMigrate(&Users{})
+	return err
+}
