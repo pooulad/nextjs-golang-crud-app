2520223d98f3d6a07add2197fbb68734c68180d5
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-07 16:10:46 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/pooulad/nextjs-golang-crud-app/bootstrap"
+	"github.com/pooulad/nextjs-golang-crud-app/repository"
+)
+
+
+type Repository repository.Repository
+
+func main() {
+	app := fiber.New()
+	bootstrap.InitializeApp(app)
 }
