f293e9f973569c00971b451a5f791676834b4c84
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-07 16:10:54 +0330

    Create routes.go

---
+++ repository/routes.go
@@ -0,0 +1,16 @@
+package repository
+
+import "github.com/gofiber/fiber/v2"
+
+
+func (repo *Repository) SetupRoutes(app *fiber.App) {
+	//routes.Setup(repo, app)
+	app.Static("/", "./client/public")
+
+	api := app.Group("/api")
+	api.Get("/users", repo.GetUsers)
+	api.Post("/users", repo.CreateUser)
+	api.Patch("/users/:id", repo.UpdateUser)
+	api.Delete("/users/:id", repo.DeleteUser)
+	api.Get("/users/:id", repo.GetUserByID)
+}
