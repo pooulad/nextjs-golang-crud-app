941272b920f35f32c5e12e2fb305e9211ca3bac0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-07 16:19:03 +0330

    Delete user.go

--- database/migrations/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	Name    string `json:"name" validate:"required,min=3,max=40"`
-	Email   string `json:"email" validate:"required,email,min=6,max=32"`
-	Date    string `json:"date" validate:"required"`
-	City    string `json:"city" validate:"required,min=6,max=40"`
-	Country string `json:"country" validate:"required,min=6,max=40"`
-}
