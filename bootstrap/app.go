116812aea768b1def912f28ffc715a3fa6536fb3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-08 13:32:22 +0330

    change cors error config

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -45,7 +45,12 @@ func InitializeApp(app *fiber.App) {
 	repo := repository.Repository{
 		DB: db,
 	}
-	app.Use(cors.New(cors.Config{AllowCredentials: true}))
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowOrigins:     "*",
+        AllowCredentials: true,
+        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	repo.SetupRoutes(app)
 	listenErr := app.Listen(":8080")
 	if listenErr != nil {
