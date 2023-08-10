1e029c0d6378b0757d777460c6a04bcf1734e9d9
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-10 17:33:36 +0330

    Update user.go

--- database/models/user.go
+++ database/models/user.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
+	ID       uint   `gorm:"primarykey;autoIncrement" json:"id"`
 	Name     string `json:"name" validate:"required,min=3,max=40"`
 	Email    string `json:"email" validate:"required,email,min=6,max=32"`
 	Date     string `json:"date" validate:"required"`
 	City     string `json:"city" validate:"required,max=40"`
 	Country  string `json:"country" validate:"required,max=40"`
-	Password string `json:"password" validate:"min=8,max=40"`
+	Password string `json:"password"`
 }
