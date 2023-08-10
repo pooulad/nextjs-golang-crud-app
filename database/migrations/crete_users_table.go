d3d252668744b4fae1a0548926ca1fb75c3d3d5b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-10 23:35:22 +0330

    make username and email unique in database

--- database/migrations/crete_users_table.go
+++ database/migrations/crete_users_table.go
@@ -8,8 +8,9 @@ import (
 
 type Users struct {
 	ID       uint      `gorm:"primarykey;autoIncrement" json:"id"`
+	Username *string   `gorm:"unique" json:"username"`
 	Name     *string   `json:"name"`
-	Email    *string   `json:"email"`
+	Email    *string   `gorm:"unique" json:"email"`
 	Date     time.Time `json:"date"`
 	City     *string   `json:"city"`
 	Country  *string   `json:"country"`
