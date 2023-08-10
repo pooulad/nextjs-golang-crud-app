17b6383a53235d4349e677468204b06ca5401611
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-10 23:35:34 +0330

    add username in user model

--- database/models/user.go
+++ database/models/user.go
@@ -2,6 +2,7 @@ package models
 
 type User struct {
 	ID       uint   `gorm:"primarykey;autoIncrement" json:"id"`
+	Username string `json:"username" validate:"required,min=3,max=40"`
 	Name     string `json:"name" validate:"required,min=3,max=40"`
 	Email    string `json:"email" validate:"required,email,min=6,max=32"`
 	Date     string `json:"date" validate:"required"`
