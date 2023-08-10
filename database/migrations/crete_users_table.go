8930e79f71101f8e168b5e93a618ecd470160c7f
pooulad <86445458+pooulad@users.noreply.github.com>
2023-08-10 14:05:44 +0330

    add password column in db

--- database/migrations/crete_users_table.go
+++ database/migrations/crete_users_table.go
@@ -7,12 +7,13 @@ import (
 )
 
 type Users struct {
-	ID      uint      `gorm:"primarykey;autoIncrement" json:"id"`
-	Name    *string   `json:"name"`
-	Email   *string   `json:"email"`
-	Date    time.Time `json:"date"`
-	City    *string   `json:"city"`
-	Country *string   `json:"country"`
+	ID       uint      `gorm:"primarykey;autoIncrement" json:"id"`
+	Name     *string   `json:"name"`
+	Email    *string   `json:"email"`
+	Date     time.Time `json:"date"`
+	City     *string   `json:"city"`
+	Country  *string   `json:"country"`
+	Password *string   `json:"password"`
 }
 
 func MigrateUsers(db *gorm.DB) error {
