b0491f20515a487f5422e9a04b71ec988f6286b8
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-22 02:37:23 +0330

    Update routes.go

--- repository/routes.go
+++ repository/routes.go
@@ -1,16 +1,26 @@
 package repository
 
 import (
+	"os"
+
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/golang-jwt/jwt/v5"
 )
 
 func (repo *Repository) SetupRoutes(app *fiber.App) {
+
 	api := app.Group("/api")
+	// Unauthenticated route
 	api.Get("/users", repo.GetUsers)
 	api.Post("/user", repo.CreateUser)
 	api.Patch("/user/:id", repo.UpdateUser)
 	api.Delete("/user/:id", repo.DeleteUser)
-	api.Get("/user/:id", repo.GetUserByID)
 	api.Post("/login", repo.Login)
-	api.Get("/authentication", repo.RequireAuth, repo.Authentication)
+	// Restricted Routes
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("SECRET_JWT"))},
+	}))
+	api.Get("/user/:id", repo.GetUserByID)
+	// api.Get("/authentication", repo.RequireAuth, repo.Authentication)
 }
