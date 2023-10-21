dd3bf4bd7f3fd573ef1ceca66796933da232a705
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-22 02:55:34 +0330

    Update routes.go

--- repository/routes.go
+++ repository/routes.go
@@ -1,15 +1,14 @@
 package repository
 
 import (
-	"os"
-
-	jwtware "github.com/gofiber/contrib/jwt"
+	// "os"
+	// jwtware "github.com/gofiber/contrib/jwt"
+	// _ "github.com/golang-jwt/jwt/v5"
+	middlewares "github.com/pooulad/nextjs-golang-crud-app/middleware"
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/golang-jwt/jwt/v5"
 )
 
 func (repo *Repository) SetupRoutes(app *fiber.App) {
-
 	api := app.Group("/api")
 	// Unauthenticated route
 	api.Get("/users", repo.GetUsers)
@@ -18,9 +17,10 @@ func (repo *Repository) SetupRoutes(app *fiber.App) {
 	api.Delete("/user/:id", repo.DeleteUser)
 	api.Post("/login", repo.Login)
 	// Restricted Routes
-	app.Use(jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("SECRET_JWT"))},
-	}))
-	api.Get("/user/:id", repo.GetUserByID)
+	// app.Use(jwtware.New(jwtware.Config{
+	// 	SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("SECRET_JWT"))},
+	// }))
+	jwtFunc := middlewares.NewAuthMiddleware()
+	api.Get("/user/:id",jwtFunc, repo.GetUserByID)
 	// api.Get("/authentication", repo.RequireAuth, repo.Authentication)
 }
