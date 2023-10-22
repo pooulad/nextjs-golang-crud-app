3a60e5cff2134e0c8d351b56695102c8079d27a3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-22 03:36:47 +0330

    start develop client dir

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,34 +2,36 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { GetServerSidePropsContext } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [state, setState] = useState<any>([]);
-  const getData = () => {
-    axios
-      .get("http://localhost:8080/api/users")
-      .then((res) => {
-        setState(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  console.log("i am:", state);
-  useEffect(() => {
-    getData();
-  }, []);
+export default function Home({users}:any) {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="w-50 p-10 bg-sky-600 text-white rounded-md">
-        {state?.data?.items?.map((item: any) => {
+        {users?.data?.items?.map((item: any) => {
           return <li>{item.email}</li>;
         })}
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // const users = await axios.get("http://localhost:8080/api/users");
+  // console.log(users);
+  const res = await fetch('http://127.0.0.1:8080/api/users');
+  const users = await res.json();
+  
+  if (!users)
+    return {
+      props: {},
+
+      redirect: { destination: "/login" },
+    };
+
+  return { props: { users } };
 }
