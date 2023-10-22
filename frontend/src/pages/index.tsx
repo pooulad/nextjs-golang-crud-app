972dbc43fa88de3a936b18020e569f55fdd367af
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-23 01:37:07 +0330

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home({ users }: any) {
       <h1 className="mb-3">All Users</h1>
       <div className="w-full grid grid-cols-1 gap-2 md:grid-cols-2">
         {users?.data?.items?.map((item: any) => {
-          return <li className="w-full mx-auto p-10 bg-sky-600 text-white rounded-md">user email : {item.email}</li>;
+          return <li key={item.id} className="w-full mx-auto p-10 bg-sky-600 text-white rounded-md">user email : {item.email}</li>;
         })}
       </div>
     </main>
