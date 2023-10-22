c910a082305cce72c43b90d2a13d77be250fc800
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-23 01:35:47 +0330

    Update useLocalStorage.ts

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -2,15 +2,16 @@ import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key);
-    if (jsonValue != null) {
-      return JSON.parse(jsonValue);
-    }
-
-    if (typeof initialValue === "function") {
-      return initialValue as () => T;
-    } else {
-      return initialValue;
+    if (typeof window !== "undefined") {
+      const jsonValue = localStorage.getItem(key);
+      if (jsonValue != null) {
+        return JSON.parse(jsonValue);
+      }
+      if (typeof initialValue === "function") {
+        return initialValue as () => T;
+      } else {
+        return initialValue;
+      }
     }
   });
   useEffect(() => {
