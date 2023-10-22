094fd0c3f5d48b8ba6383789d27d59fdfacb08be
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-23 01:40:43 +0330

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,9 @@
 import { Container, Navbar as NavbarBs, Nav } from "react-bootstrap";
-import { useRouter } from "next/router";
 import { useUserContext } from "../context/UserContext";
-import { ReactElement } from "react";
 import Link from "next/link";
 
 function Navbar() {
   const { userData } = useUserContext();
-  const router = useRouter();
-  console.log(userData);
   return (
     <NavbarBs className="bg-dark text-light mb-3">
       <Container>
