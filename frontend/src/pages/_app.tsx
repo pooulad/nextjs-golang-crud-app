5e3fd341026b97141d11e5c9916179e5ff2c638d
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-23 01:35:50 +0330

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from "@/components/Navbar";
+import { UserProvider } from "@/context/UserContext";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
