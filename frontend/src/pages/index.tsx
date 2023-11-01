b296697c47051a63295712e053dd0ef96876747a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-11-02 01:51:19 +0330

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
+"use client";
 import axios from "axios";
-import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import configJson from "../config.json";
 import Link from "next/link";
