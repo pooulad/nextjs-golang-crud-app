a8664b5ba9e7dee7c5d230c72eadc130b31c92e6
pooulad <poooooladi@gmail.com>
2023-11-01 18:15:53 +0330

    Update new.tsx

--- frontend/src/pages/new.tsx
+++ frontend/src/pages/new.tsx
@@ -43,13 +43,7 @@ function NewUserPage() {
               type="button"
             >
               Sign In
-            </button>
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
+            </button> 
           </div>
         </form>
         <p className="text-center text-gray-500 text-xs">
