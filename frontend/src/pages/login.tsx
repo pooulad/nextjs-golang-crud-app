c7362cb48bc03537748c1685d7337276dc7348a0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-11-02 01:00:03 +0330

    create login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,8 +1,124 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import configJson from "../config.json";
+import { ToastErrorMessage, ToastSuccessMessage } from "@/utils/ToastGenerator";
+import { useRouter } from "next/navigation";
+import ReactLoading from 'react-loading';
+
 
 function LoginPage() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  type Inputs = {
+    name: string;
+    username: string;
+    password: string;
+    date: string;
+    country: string;
+    city: string;
+    email: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setLoading(true);
+    axios
+      .post(`${configJson.localApi}/user`, data)
+      .then((res) => {
+        if (res.status === 200) {
+          ToastSuccessMessage(res.data.message);
+          setTimeout(() => {
+            router.push("/");
+          }, 3000);
+        }
+      })
+      .catch((err) => {
+        ToastErrorMessage(err + " Error happend. maybe username or email is duplicate...");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
-    <main className={`flex min-h-screen flex-col items-center p-4`}></main>
+    <main className={`flex min-h-screen flex-col items-center p-4`}>
+      {loading ? (
+        <>
+          <ReactLoading type={"bars"} color={"red"} height={200} width={200} className="flex flex-row min-h-screen justify-center items-center" />
+        </>
+      ) : (
+        <>
+          <div className="w-full max-w-xs">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Username
+                </label>
+                <input
+                  className={`shadow appearance-none border ${
+                    errors.username && "border-red-500"
+                  } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  {...register("username", { required: true, min: 3 })}
+                />
+                {errors.username && (
+                  <p className="text-red-500 text-xs italic">
+                    Please choose a username.
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className={`shadow appearance-none border ${
+                    errors.password && "border-red-500"
+                  } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+                  id="password"
+                  type="password"
+                  placeholder="******************"
+                  {...register("password", { required: true })}
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-xs italic">
+                    Please choose a password.
+                  </p>
+                )}
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            <p className="text-center text-gray-500 text-xs">
+              &copy;2020 Acme Corp. All rights reserved.
+            </p>
+          </div>
+        </>
+      )}
+    </main>
   );
 }
 
