7da5143e271c2606d4f2504c469d059a94e23b2a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-11-02 00:12:09 +0330

    add react toastify

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <ToastContainer />
     </>
   );
 }
