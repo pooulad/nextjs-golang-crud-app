45124078f0a11417d31cb4dbf3001a159675bde9
pooulad <poooooladi@gmail.com>
2023-11-01 16:54:59 +0330

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,6 @@
-import Navbar from "@/components/Navbar";
-import { UserProvider } from "@/context/UserContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <UserProvider>
-      <Navbar />
-      <Component {...pageProps} />
-    </UserProvider>
-  );
+  return <Component {...pageProps} />;
 }
