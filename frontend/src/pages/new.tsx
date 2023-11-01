a8f4437b0ed6821bb6e22dea362db14c17b5276e
pooulad <86445458+pooulad@users.noreply.github.com>
2023-11-01 23:44:34 +0330

    complete form ui

--- frontend/src/pages/new.tsx
+++ frontend/src/pages/new.tsx
@@ -1,11 +1,33 @@
 "use client";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 function NewUserPage() {
+  type Inputs = {
+    name: string;
+    username: string;
+    password: string;
+    date: string;
+    country: string;
+    city: string;
+    email: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {};
   return (
     <main className={`flex min-h-screen flex-col items-center p-4`}>
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -14,11 +36,107 @@ function NewUserPage() {
               Username
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border ${
+                errors.username && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
               id="username"
               type="text"
               placeholder="Username"
+              {...register("username", { required: true })}
             />
+            {errors.username && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a username.
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className={`shadow appearance-none border ${
+                errors.name && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+              id="name"
+              type="text"
+              placeholder="Name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a name.
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="city"
+            >
+              City
+            </label>
+            <input
+              className={`shadow appearance-none border ${
+                errors.city && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+              id="city"
+              type="text"
+              placeholder="Name"
+              {...register("city", { required: true })}
+            />
+            {errors.city && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a city.
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="country"
+            >
+              Country
+            </label>
+            <input
+              className={`shadow appearance-none border ${
+                errors.country && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+              id="country"
+              type="text"
+              placeholder="Name"
+              {...register("country", { required: true })}
+            />
+            {errors.country && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a country.
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="date"
+            >
+              Date
+            </label>
+            <input
+              className={`shadow appearance-none border ${
+                errors.date && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+              id="date"
+              type="date"
+              placeholder="Date"
+              {...register("date", { required: true })}
+            />
+            {errors.date && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a date.
+              </p>
+            )}
           </div>
           <div className="mb-6">
             <label
@@ -28,22 +146,27 @@ function NewUserPage() {
               Password
             </label>
             <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border ${
+                errors.password && "border-red-500"
+              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
               id="password"
               type="password"
               placeholder="******************"
+              {...register("password", { required: true })}
             />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            )}
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign In
-            </button> 
+            </button>
           </div>
         </form>
         <p className="text-center text-gray-500 text-xs">
