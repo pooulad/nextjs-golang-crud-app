fbc80a4456b837db1404a9ea26a8dcb5a319398b
pooulad <poooooladi@gmail.com>
2025-02-01 21:13:52 +0330

    Test dockerizing project

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,5 +1,5 @@
 import { SiteConfig } from "../types/index";
 
 export const siteConfig: SiteConfig = {
-  localApi: "http://127.0.0.1:8080/api",
+  localApi: "http://127.0.0.1:8000/api",
 };
--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,71 +1,70 @@
-import { getServerSession, NextAuthOptions } from "next-auth"
+import { getServerSession, NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions: NextAuthOptions = {
-    session: {
-      strategy: "jwt",
-    },
-    providers: [
-      // EmailProvider
-      // CustomProvider
-      CredentialsProvider({
-        name: "Credentials",
-  
-        credentials: {
-          username: { label: "Username", type: "text", placeholder: "jsmith" },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials, req) {
-          // Add logic here to look up the user from the credentials supplied
-  
-          const res = await fetch("http://localhost:8080/api/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              username: credentials?.username,
-              password: credentials?.password,
-            }),
-          });
-          const user = await res.json();
-          console.log("this is user",user);
-          console.log("this is req",req);
-          
-  
-          if (user) {
-            return user;
-          } else {
-            return null;
-          }
-        },
-      }),
-    ],
-    callbacks: {
-      async jwt({ token, user }) {
-        return { ...token, ...user };
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    // EmailProvider
+    // CustomProvider
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
       },
-      async session({ session, token, user }) {
-        console.log("sd",user);
-        
-        session.user = token as any;
-        return session;
-      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+
+        const res = await fetch(`http://127.0.0.1:8000/api/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+        const user = await res.json();
+        console.log("this is user", user);
+        console.log("this is req", req);
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },      
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
     },
-    pages: {
-      signIn: "/auth/signin",
-      // signOut: "/auth/signout",
-      // error: "/auth/error",
-      // verifyRequest: "/auth/verify-request",
-      // newUser: "/auth/new-user",
+    async session({ session, token, user }) {
+      console.log("sd", user);
+
+      session.user = token as any;
+      return session;
     },
-    debug: true,
-  };
+  },
+  pages: {
+    signIn: "/auth/signin",
+    // signOut: "/auth/signout",
+    // error: "/auth/error",
+    // verifyRequest: "/auth/verify-request",
+    // newUser: "/auth/new-user",
+  },
+  debug: true,
+};
 
 /**
  * Helper function to get the session on the server without having to import the authOptions object every single time
  * @returns The session object or null
  */
-const getSession = () => getServerSession(authOptions)
+const getSession = () => getServerSession(authOptions);
 
-export { authOptions, getSession }
+export { authOptions, getSession };
